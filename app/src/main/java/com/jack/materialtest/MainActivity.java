254309ff46d1e68c5d2d865bc5271415844390f4
ych1147471053 <1147471053@qq.com>
2019-09-24 13:47:04 +0800

    添加NavigationView

--- app/src/main/java/com/jack/materialtest/MainActivity.java
+++ app/src/main/java/com/jack/materialtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jack.materialtest;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 //import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,11 +31,24 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+
+        NavigationView navView=(NavigationView)findViewById(R.id.nav_view);
+
+
         ActionBar actionBar=getSupportActionBar();
         if (actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+
+        navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
 
 
     }
